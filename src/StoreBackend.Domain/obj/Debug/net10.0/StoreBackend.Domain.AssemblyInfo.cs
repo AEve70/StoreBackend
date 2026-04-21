74e5e7219a944c487dac3cbf497c23396bb771d2
Evelyn <evemh19@gmail.com>
2026-04-20 20:43:15 -0600

    Update assembly informational versions and editor config settings across multiple projects

--- src/StoreBackend.Domain/obj/Debug/net10.0/StoreBackend.Domain.AssemblyInfo.cs
+++ src/StoreBackend.Domain/obj/Debug/net10.0/StoreBackend.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreBackend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5c6e6ce1518c6a35012a079d8f76385000974f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa94827ebd644c43598b9bfb41b6408bb00d783")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreBackend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreBackend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
